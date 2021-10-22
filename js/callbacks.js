3e46f74fb8ec9013aba2313151b461f39bd2526c
michael <michael974.parmentier@gmail.com>
2021-10-22 09:03:45 +0200

    Update HTML JS

--- js/callbacks.js
+++ js/callbacks.js
@@ -82,30 +82,7 @@ function handleMouseMove(event) {
 	lastMouseY = newY;
 }
 
-// =====================================================
-String.prototype.convertToRGBA = function hexToRgbA(hex){
-    var c;
-    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-        c= hex.substring(1).split('');
-        if(c.length== 3){
-            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-        c= '0x'+c.join('');
-        return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',1)';
-    }
-    throw new Error('Bad Hex');
-}
-
 // ====================================================
-function hexToRgb(hex) {
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-	  r: parseInt(result[1], 16),
-	  g: parseInt(result[2], 16),
-	  b: parseInt(result[3], 16)
-	} : null;
-  }
-
   function convertRGBA(value) {
 	  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
 	  return result ? [
