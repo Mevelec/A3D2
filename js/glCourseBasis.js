9a545867597f07853d8ea4a2e7ea7620a94cdb7f
michael <michael974.parmentier@gmail.com>
2021-12-08 09:43:12 +0100

    Suppression des elts inutiles

--- js/glCourseBasis.js
+++ js/glCourseBasis.js
@@ -194,59 +194,8 @@ class plane {
 		this.loaded=-1;
 		this.shader=null;
 		this.initAll();
-		this.loadTexture();
 	}
 
-	setTexcoords(gl) {
-		console.log("setTexcoords");
-		gl.bufferData(
-			gl.ARRAY_BUFFER,
-			new Float32Array([
-			  // left column front
-			  0, 0,
-			  0, 1,
-			  1, 0,
-			  0, 1,
-			  1, 1,
-			  1, 0,
-	   
-			  // top rung front
-			  0, 0,
-			  0, 1,
-			  1, 0,
-			  0, 1,
-			  1, 1,
-			  1, 0,
-			 ]),
-			 gl.STATIC_DRAW);
-	}
-
-
-	loadTexture(){
-		console.log("loadTexture");
-		// Create a texture.
-		var texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		
-		// Fill the texture with a 1x1 blue pixel.
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-					new Uint8Array([0, 0, 255, 255]));
-		
-		// Asynchronously load an image
-		var image = new Image();
-		image.src = "./basketball.jpg";
-		image.addEventListener('load', function() {
-			// Now that the image has loaded make copy it to the texture.
-			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
-			gl.generateMipmap(gl.TEXTURE_2D);
-		});
-
-		console.log(image);
-
-		console.log("Texture loaded");
-	}
-		
 	// --------------------------------------------
 	initAll() {
 		var size=1.0;
